155ca9e346f72236d9b89c71b77e9cad4dfa9d36
Xie wenhao <1078637049@qq.com>
2023-11-03 14:56:17 +0800

    新建vue文件

---
+++ app-vue3/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    {{ name }}
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {      
+      name: "aaa",
+    };
+  },
+};
+</script>
+<style scoped lang="scss"></style>
